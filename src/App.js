d5424903f04441639844603fbe9fec26f31132a2
Aleksandra Pankratova <aleksandra_pankratova@epam.com>
2017-04-14 18:25:41 +0300

    - Add alternative way for 'Add.js'

--- src/App.js
+++ src/App.js
@@ -2,4 +2,11 @@ import React from 'react';
 
 const App = () => <h1>Hi</h1>;
 
+//Alternative way
+/*class App extends React.Component {
+    render() {
+        return <h1>Hi</h1>
+    }
+}*/
+
 export default App;
