15e15a111a1105e46606909c00f8f1514f740f48
Aleksandra Pankratova <aleksandra_pankratova@epam.com>
2017-04-17 12:33:07 +0300

    - Add work with states

---
+++ src/WithState.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class WithState extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            txt: 'this is the state text',
+            cat: 0
+        }
+    }
+
+    update(e) {
+        this.setState({txt: e.target.value})
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" onChange={this.update.bind(this)}/>
+                <h1>{this.state.txt} - {this.state.cat}</h1>
+            </div>
+        )
+    }
+}
+
+export default WithState;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import HelloMessage from './Hello';
 // import App from './App';
-import WithProps from './WithProps';
+// import WithProps from './WithProps';
+import WithState from './WithState';
 
 ReactDOM.render(
     <div>
         {/*<HelloMessage name="Marty" />*/}
         {/*<App />*/}
-        <WithProps cat={5} txt="this is the prop text" />
+        {/*<WithProps cat={5} txt="this is the prop text" />*/}
         {/*<WithProps txt="this is the prop value" />*/}
+        <WithState />
     </div>,
   document.getElementById('root')
 );
